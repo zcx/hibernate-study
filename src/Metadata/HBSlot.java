000049bdd15565b56c358e109e218969889b9dba
classfoo <juanzhewudi@gmail.com>
2011-02-28 11:46:57 +0800

    ISSUE0001 为HBSlot增加与HBObject的多对一关联，这个提交也是为了体验git的尽量创建分支的特性

--- src/Metadata/HBSlot.java
+++ src/Metadata/HBSlot.java
@@ -4,11 +4,21 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
 import org.hibernate.annotations.GenericGenerator;
 
+/**
+ * 通过Hibernate构建的元数据Slot存储测试验证类
+ * 20110228
+ *   ISSUE0001 为HBSlot增加owner，这是一个多对一的关系，使得多个HBSlot能够关联到一
+ *   个HBObject中，这个提交主要也是为了测试git中尽量频繁创建分支的概念
+ * @author classfoo
+ *
+ */
 @Entity
 @Table(name="DB_HBSLOT")
 public class HBSlot {
@@ -22,6 +32,10 @@ public class HBSlot {
 	@Version
 	@Column(name="VERSION")
 	private int version;
+	
+	@ManyToOne
+	@JoinColumn(name="OBJECTID")
+	private HBObject owner = null;
 	
 	public String getID(){
 		return this.id;
@@ -37,6 +51,14 @@ public class HBSlot {
 
 	public int getVersion() {
 		return version;
+	}
+
+	public void setOwner(HBObject owner) {
+		this.owner = owner;
+	}
+
+	public HBObject getOwner() {
+		return owner;
 	}
 	
 	
