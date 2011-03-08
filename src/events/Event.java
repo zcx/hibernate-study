d83fd3b1730d260178b775916adddc378c45f41d
zcx <zhuchx1008@gmail.com>
2011-03-08 09:36:58 +0800

    在Hibernate学习仓库中加入一个Hibernate简单的演示示例，说明可见events/instruction.txt

---
+++ src/events/Event.java
@@ -0,0 +1,38 @@
+package events;
+
+import java.util.Date;
+
+public class Event {
+	private long id;
+
+	private String title;
+
+	private Date date;
+
+	public Event() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
